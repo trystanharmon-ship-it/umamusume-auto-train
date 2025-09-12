ddb444fb97145904e2b7e654b6b66475d992d849
Seçkin Ozan Uyan <ozanuyan@gmail.com>
2025-09-12 13:44:24 +0300

    added close button click to the status check andadded file logging with max 11mb logs

--- utils/log.py
+++ utils/log.py
@@ -1,5 +1,7 @@
 # logging tools
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -10,3 +12,15 @@ info = logging.info
 warning = logging.warning
 error = logging.error
 debug = logging.debug
+
+log_dir = os.path.join("..", "logs")
+os.makedirs(log_dir, exist_ok=True)
+
+handler = RotatingFileHandler(
+    os.path.join(log_dir, "log.txt"),
+    maxBytes=1_000_000,
+    backupCount=10,
+    encoding="utf-8"
+)
+
+logging.getLogger().addHandler(handler)
