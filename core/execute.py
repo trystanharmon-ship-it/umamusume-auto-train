b7ac3a8d1d7ff3e0724bd7bbeedbec7bab691c6a
Seff160821 <ivan.fernan2000@gmail.com>
2025-10-21 18:52:15 +0800

    Added logic for recreation to click the Uma’s name for recreation once the date_event is completed.

--- core/execute.py
+++ core/execute.py
@@ -160,12 +160,17 @@ def do_recreation():
 
     aoi_event = pyautogui.locateCenterOnScreen("assets/ui/aoi_event.png", confidence=0.8)
     tazuna_event = pyautogui.locateCenterOnScreen("assets/ui/tazuna_event.png", confidence=0.8)
-    if tazuna_event:
-      pyautogui.moveTo(tazuna_event, duration=0.15)
-      pyautogui.click(tazuna_event)
+    date_complete = pyautogui.locateCenterOnScreen("assets/ui/date_complete.png", confidence=0.8)
+
+    if date_complete:
+      pyautogui.moveTo(410, 500, duration=0.15)
+      pyautogui.click()
     elif aoi_event:
       pyautogui.moveTo(aoi_event, duration=0.15)
       pyautogui.click(aoi_event)
+    elif tazuna_event:
+      pyautogui.moveTo(tazuna_event, duration=0.15)
+      pyautogui.click(tazuna_event)
   elif recreation_summer_btn:
     click(boxes=recreation_summer_btn)
 
